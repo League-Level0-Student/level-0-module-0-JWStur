0c5a3766a35551f4dca91715a64d6c80b5a3f1d7
root <root@fd96273d312a>
2020-05-30 21:30:57 +0000

    Im working on the Scary Maze

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,9 +1,12 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
+		int score = 0;
 		
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
@@ -13,11 +16,36 @@ public class QuizGame {
 				
 				// 4.  if the user's answer was correct, add one to their score 
 		
+		String input = JOptionPane.showInputDialog("How many toes does a fish have? ");
+		
+		if (input.equals("0")) {
+			score +=1;
+			
+		}
+		else {
+			score -= 1;
+			
+		}
+		
+		input = JOptionPane.showInputDialog("How many wings are on a cow? ");
+		
+		if (input.equals("0")) {
+			score += 1;
+			
+		}
+		else {
+			score -= 1;
+			
+		}
+		
+		JOptionPane.showMessageDialog(null, "You scored " + score);
+	
+		
+		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
 		
-		
-		// After all the questions have been asked, tell the user their final score 
+// After all the questions have been asked, tell the user their final score 
 		
 	}
 }
